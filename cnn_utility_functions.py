d120bd60d00a9f16d4acd89d2fc0fc7b402e7fbf
Luke Wilson <luke.a.h.wilson@gmail.com>
2021-10-31 16:15:28 -0700

    Large File Error, Reset and Squash Changes, cnn_utility_functions completed
     - Annotated the utility functions
     - Improved data loader function to work without all types of data

--- cnn_utility_functions.py
+++ cnn_utility_functions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # PROGRAMMER: Luke Wilson
 # DATE CREATED: 2021-10-12
-# REVISED DATE: 2021-10-27
+# REVISED DATE: 2021-01-01
 # PURPOSE: Provide utility functions for import into main
 #   - u1_get_input_args()
 #   - u2_load_processed_data(data_dir)
@@ -11,7 +11,7 @@
 #   - u6_user_input_prompt(prompt, default)
 ##
 
-# Import required libraries
+# Import libraries
 import json
 import argparse
 import os, random
@@ -26,7 +26,9 @@ from threading import Thread
 
 def u1_get_input_args():
     '''
-    Creates and stores command line arguments inputted by the user. Attaches default arguments and help text to aid user.
+    Purpose:
+        - Creates and stores command line arguments inputted by the user.
+        - Attaches default arguments and help text to aid user.
     Command Line Arguments:
         1. Data directory as --dir
         2. Choose to load model as --load
@@ -38,18 +40,16 @@ def u1_get_input_args():
     Returns:
         - Stored command line arguments as an Argument Parser Object with parse_args() data structure
     '''
-
     parser = argparse.ArgumentParser(description = 'Classify input images and benchmark performance')
-    parser.add_argument('--dir', type=str, default= os.path.expanduser('~')+'/Programming Data/Flower_data/', help='input path for data directory')
+    parser.add_argument('--dir', type=str, default= 'Flower_data/', help='input path for data directory')
     parser.add_argument('--load', type=str, default='n', help='yes \'y\' or no \'n\' to load state_dict for model', choices=['y','n'])
     parser.add_argument('--train', type=str, default='n', help='yes \'y\' or no \'n\' to retrain this model', choices=['y','n'])
-    parser.add_argument('--epoch', type=int, default=100, help='provide the number of epochs for training (default 100)')
+    parser.add_argument('--epoch', type=int, default=50, help='provide the number of epochs for training (default 100)')
     parser.add_argument('--layer', type=int, default=2, help='provide the number of hidden layers to use (default 2)')
     parser.add_argument('--learn', type=int, default=0.003, help='provide the learning rate to begin training (default 0.003)')
     parser.add_argument('--model', type=str, default='googlenet', help='select pretrained model',
                         choices=['vgg', 'alexnet', 'googlenet', 'densenet', 'resnext', 'shufflenet'])
-
-    return parser.parse_args() #return parsed arguments
+    return parser.parse_args()
 
 
 def u2_load_processed_data(data_dir):
@@ -64,37 +64,45 @@ def u2_load_processed_data(data_dir):
         - dictionary of data labels
         - dictionary of class labels
     '''
-
+    # Initialize empty dictionaries to hold data and data labels
     dict_datasets = {}
+    dict_data_labels = {}
+
+    # Iterate through folders in the data directory
     for folder in os.listdir(data_dir):
-        if os.path.splitext(folder)[1] == '' and folder != 'predict':
+
+        # If data exists, create datasets for overfitting, testing, training, and validating data
+        if folder in ['overfit', 'test', 'train', 'valid']:
             dict_datasets[folder + '_data'] = datasets.ImageFolder(data_dir + folder, transform=u3_process_data(folder))
-        if os.path.splitext(folder)[1] == '' and folder == 'predict':
+
+        # If data for inference exists, create a dataset from the predict folder
+        if folder == 'predict':
             predict_transform = u3_process_data(folder)
-            dict_datasets['predict_data'] = \
-                            [(predict_transform(Image.open(data_dir + folder + '/' + filename)), filename)\
-                            for filename in os.listdir(data_dir + folder)]
+            dict_datasets['predict_data'] = [(predict_transform(Image.open(data_dir + folder + '/' + filename)),
+                            filename) for filename in os.listdir(data_dir + folder)]
+
+        # If a data names are added to the data directory as a json, open it and read into data label dictionary
         if os.path.splitext(folder)[1] == '.json':
             with open(data_dir + folder, 'r') as f:
                 dict_data_labels = json.load(f)
-    dict_class_labels = {value : key for (key, value) in dict_datasets['train_data'].class_to_idx.items()}
 
+    # Create a dictionary connecting class indexes to class labels, return the datasets and label dictionaries
+    dict_class_labels = {value : key for (key, value) in dict_datasets['train_data'].class_to_idx.items()}
     return dict_datasets, dict_data_labels, dict_class_labels
 
 
 def u3_process_data(transform_request):
     '''
     Purpose:
-        - Provide an assortment of transforms for application to specific datasets
+        - Define an assortment of transforms for application to specific datasets
         - Return the appropriate transformation that corresponds to the inputted request
+        - Defined transforms are composed of a sequence of individual transform operations
+        - Depending on the needs of each data set, a transform will use specific operations
     Parameters:
         - transformation_request = selected transformation type
     Returns:
         - transform that corresponds to the request
     '''
-    #Define transforms for training, validation, overfitting, and test sets to convert to desirable tensors for processing
-
-    #Define image size
     image_1d_size = 224
 
     predict_transform = transforms.Compose([transforms.Resize(int(np.round_(image_1d_size*1.1, decimals=0))),
@@ -127,7 +135,6 @@ def u3_process_data(transform_request):
                                          transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
     overfit_transform = train_transform
-
     return locals()[transform_request + '_transform']
 
 
@@ -141,15 +148,11 @@ def u4_data_iterator(dict_datasets):
         - dict_datasets = dictionary of datasets
     Returns:
         - dict_data_loaders = dictionary of dataloaders
-    # bug, requires every folder to run correctly. Consider removing this function and nesting it directly into training function!
     '''
     dict_data_loaders = {}
-    dict_data_loaders['train_loader'] = torch.utils.data.DataLoader(dict_datasets['train_data'], batch_size=128, shuffle=True)
-    dict_data_loaders['valid_loader'] = torch.utils.data.DataLoader(dict_datasets['valid_data'], batch_size=128, shuffle=True)
-    dict_data_loaders['testing_loader'] = torch.utils.data.DataLoader(dict_datasets['test_data'], batch_size=128, shuffle=True)
-    dict_data_loaders['overfit_loader'] = torch.utils.data.DataLoader(dict_datasets['overfit_data'], batch_size=8, shuffle=True)
-    dict_data_loaders['predict_loader'] = torch.utils.data.DataLoader(dict_datasets['predict_data'], batch_size=128, shuffle=False)
-
+    for dataset in dict_datasets:
+        loader_type = dataset.split('_')[0] + '_loader'
+        dict_data_loaders[loader_type] = torch.utils.data.DataLoader(dict_datasets[dataset], batch_size=128, shuffle=True)
     return dict_data_loaders
 
 
@@ -168,11 +171,10 @@ def u5_time_limited_input(prompt, default=True):
     TIMEOUT = 10
     prompt = prompt + f': \'y\' for yes, \'n\' for no ({TIMEOUT} seconds to choose): '
     user_input_thread = Thread(target=u6_user_input_prompt, args=(prompt, default), daemon = True)
-    user_input_thread.start()
-    user_input_thread.join(TIMEOUT)
+    user_input_thread.start() # Start the thread, calling the user input function
+    user_input_thread.join(TIMEOUT) # Limit the thread to the TIMEOUT time limit
     if not answered:
         print('\n No valid input, proceeding with operation...')
-
     return choice
 
 
@@ -188,7 +190,7 @@ def u6_user_input_prompt(prompt, default):
     Returns:
         - choice = the user input or the default
     '''
-    global choice, answered
+    global choice, answered # Global variables are required to communicate input statuses back to the thread manager
     choice = default
     answered = False
     while not answered:
